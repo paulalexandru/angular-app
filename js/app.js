4ccfaf8477b26507b071aa5c8313f04d293d31df
paulalexandru <paul.ilea02@gmail.com>
2016-06-17 02:25:31 +0300

    Finish Shaping up with Angular.js

--- js/app.js
+++ js/app.js
@@ -1,22 +1,14 @@
 (function(){
-    var app = angular.module('store', []);
+    var app = angular.module('store', ['store-products']);
 
-    app.controller('StoreController', function(){
-        this.products = product;
-    });
+    app.controller('StoreController', ['$http', function($http){
+        var store = this;
+        store.products = [];
 
-    app.controller("PanelController", function(){
-      this.tab = 1;
-
-      this.selectTab = function(setTab){
-        this.tab = setTab;
-      }
-
-      this.isSelected = function(checkTab){
-        return this.tab === checkTab;
-      }
-
-    });
+        $http.get('service/products.json').success(function(data){
+          store.products = data;
+        });
+    }]);
 
     app.controller("ReviewController", function(){
       this.review = {};
@@ -28,59 +20,6 @@
     });
 
     var product = [
-      {
-        name: 'Dodecahedron',
-        price: 2.95,
-        description: 'Some gems have hidden qualities beyond their luster.',
-        canPurchase: true,
-        soldOut: false,
-        images: [
-            {
-              full: 'gem1-full.jpg',
-            },
-            {
-              thumb: 'gem2-thumb.jpg',
-            }
-        ],
-        reviews: [
-          {
-            stars: 5,
-            body: "I love this product!",
-            author: "joe@thomas.com"
-          },
-          {
-            stars: 1,
-            body: "This product sucks!",
-            author: "tim@hater.com"
-          }
-        ]
-      },
-      {
-        name: 'Pentagonal Gem',
-        price: 5.95,
-        description: 'Some gems have hidden qualities beyond their luster.',
-        canPurchase: true,
-        soldOut: false,
-        images: [
-            {
-              full: 'gem2-full.jpg',
-            },
-            {
-              thumb: 'gem2-thumb.jpg',
-            }
-        ],
-        reviews: [
-          {
-            stars: 1,
-            body: "This product sucks!",
-            author: "joe@thomas.com"
-          },
-          {
-            stars: 5,
-            body: "I love this product!",
-            author: "tim@hater.com"
-          }
-        ]
-      }
+
     ];
 })()
---
+++ js/products.js
@@ -0,0 +1,44 @@
+(function(){
+    var app = angular.module('store-products', []);
+
+    app.directive('productTitle', function(){
+        return {
+          restrict: 'E',
+          templateUrl: 'product-title.html'
+        };
+    });
+
+    app.directive('productPanels', function(){
+      return {
+        restrict: 'E',
+        templateUrl: 'product-panels.html',
+        controller: function(){
+          this.tab = 1;
+
+          this.selectTab = function(setTab){
+            this.tab = setTab;
+          }
+
+          this.isSelected = function(checkTab){
+            return this.tab === checkTab;
+          }
+        },
+        controllerAs: 'panel'
+      };
+    });
+
+    app.directive("productGallery", function() {
+      return {
+        restrict: "E",
+        templateUrl: "product-gallery.html",
+        controller: function() {
+          this.current = 0;
+          this.setCurrent = function(imageNumber){
+            this.current = imageNumber || 0;
+          };
+        },
+        controllerAs: "gallery"
+      };
+    });
+
+})()
