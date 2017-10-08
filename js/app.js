c1f1fbb65fe21be8e1d47f857faf866658880815
paulalexandru <paul.ilea02@gmail.com>
2017-10-08 22:02:01 +0300

    Add app

--- js/app.js
+++ js/app.js
@@ -10,16 +10,12 @@
         });
     }]);
 
-    app.controller("ReviewController", function(){
-      this.review = {};
+    app.controller('ReviewController', function(){
+        this.review = {};
 
-      this.addReview = function(product){
-          product.reviews.push(this.review);
-          this.review = {};
-      };
+        this.addReview = function(product){
+            product.reviews.push(this.review);
+            this.review = {};
+        }
     });
-
-    var product = [
-
-    ];
-})()
+})();
--- js/products.js
+++ js/products.js
@@ -1,44 +1,48 @@
 (function(){
     var app = angular.module('store-products', []);
 
+    app.controller('PanelController', function(){
+        this.tab = 1;
+
+        this.selectTab = function(setTab){
+            this.tab = setTab;
+        }
+
+        this.isSelected = function(checkTab){
+            return this.tab === checkTab;
+        }
+    });
+
+    app.controller('GalleryController', function(){
+        this.current = 0;
+
+        this.setCurrent = function(imageNumber){
+            this.current = imageNumber || 0;
+        }
+    });
+
     app.directive('productTitle', function(){
         return {
-          restrict: 'E',
-          templateUrl: 'product-title.html'
-        };
+            restrict: 'E',
+            templateUrl: 'templates/product-title.html'
+        }
     });
 
     app.directive('productPanels', function(){
-      return {
-        restrict: 'E',
-        templateUrl: 'product-panels.html',
-        controller: function(){
-          this.tab = 1;
-
-          this.selectTab = function(setTab){
-            this.tab = setTab;
-          }
-
-          this.isSelected = function(checkTab){
-            return this.tab === checkTab;
-          }
-        },
-        controllerAs: 'panel'
-      };
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/product-panels.html',
+            controller: 'PanelController',
+            controllerAs: 'panel'
+        }
     });
 
-    app.directive("productGallery", function() {
-      return {
-        restrict: "E",
-        templateUrl: "product-gallery.html",
-        controller: function() {
-          this.current = 0;
-          this.setCurrent = function(imageNumber){
-            this.current = imageNumber || 0;
-          };
-        },
-        controllerAs: "gallery"
-      };
-    });
-
-})()
+    app.directive('productGallery', function(){
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/product-gallery.html',
+            controller: 'GalleryController',
+            controllerAs: 'gallery'
+        };
+    })
+})();
